dbdb88dc648ccf373be0d571b9acc1dee42b86f7
neoguye <neoguye@live.com>
2017-07-01 21:57:33 -0400

    Update 10_python_challenge.py

--- Python/10_python_challenge.py
+++ Python/10_python_challenge.py
@@ -1,5 +1,13 @@
 #Challenge
 #Find the numbers whıch can dive 4 and 4 multiple(tetragenous) in from 0 to 100
+
+#ive only started ( im on the lesson 10 assignment and i got
+
+for n in range (0,101,4):
+    print(n)
+
+'''
 for n in range(101):
     if(n % 4 is 0):
         print(n)
+        '''
